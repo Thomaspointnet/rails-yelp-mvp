f5998c3d8aabb4ad119b22ea13912621d655fad9
Thomas Petitprez <t.petitprez@outlook.fr>
2021-06-09 16:24:07 +0200

    rake 100% OK

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,8 +8,12 @@ class RestaurantsController < ApplicationController
   end
 
   def create
-    restaurant = Restaurant.new(restaurant_params)
-    redirect_to restaurant_path(restaurant) if restaurant.save
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
